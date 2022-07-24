1c01080854fb71f0e87a521776c41ac757d092ec
Anton Moskalenko <moskalenko.anton@gmail.com>
2022-07-24 16:59:19 +0300

    Add Meta::getList method.

--- Source/Dumper/Meta.php
+++ Source/Dumper/Meta.php
@@ -2,6 +2,8 @@
 
 namespace Liloi\Dumper;
 
+use Liloi\Dumper\Atom;
+
 /**
  * Meta class.
  * @package Liloi\Dumper
@@ -15,7 +17,59 @@ class Meta
      */
     public static function getList(): array
     {
+
+        $dirMeta = __DIR__ . '/../Meta';
+        $fnsAtoms = self::getDirContents($dirMeta);
+
         $list = [];
+
+        foreach ($fnsAtoms as $fn)
+        {
+            $data = json_decode(file_get_contents($fn), true);
+
+            foreach($data as $datum)
+            {
+                $list[] = new Atom($datum);
+            }
+        }
+
+        return $list;
+    }
+
+    /**
+     * Gets atom JSON list.
+     *
+     * @param string $dir Find at directory.
+     * @return array Atom JSON list.
+     */
+    private static function getDirContents(string $dir): array
+    {
+        $list = [];
+        $files = scandir($dir);
+
+        foreach($files as $value)
+        {
+            if(in_array($value, ['.', '..']))
+            {
+                continue;
+            }
+
+            $filename = $dir . DIRECTORY_SEPARATOR . $value;
+
+            if(!is_dir($filename))
+            {
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+                if(in_array($ext, ['json', 'atom']))
+                {
+                    $list[] = $filename;
+                }
+
+                continue;
+            }
+
+            $list = array_merge($list, self::getDirContents($filename));
+        }
 
         return $list;
     }
